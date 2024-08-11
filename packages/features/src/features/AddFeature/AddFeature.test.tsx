776bb7b65d015c7918ff5dac9de00b12b35db957
Stewart Armbrecht (BBB) <stewart@boundbybetter.com>
2024-08-11 17:00:50 +0000

    Added cover commands for ensuring code coverage.

--- packages/features/src/features/AddFeature/AddFeature.test.tsx
+++ packages/features/src/features/AddFeature/AddFeature.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { act, fireEvent, waitFor } from '@testing-library/react-native';
+import { fireEvent, waitFor } from '@testing-library/react-native';
 import { AddFeature } from './AddFeature';
 import { useAppDispatch } from '@boundbybetter/state';
 import { renderWithTamagui } from '../../renderWithTamagui.test-util';
@@ -13,30 +13,26 @@ jest.mock('@boundbybetter/state', () => ({
 describe('AddFeature', () => {
   it('should update the key when input value changes', async () => {
     const { getByPlaceholderText } = renderWithTamagui(<AddFeature />);
-    await act(async () => {
-      const inputElement = getByPlaceholderText('New Feature Key');
-      fireEvent.changeText(inputElement, 'New Feature Key');
-      waitFor(() => expect(inputElement.props.value).toBe('New Feature Key'));
-    });
+    const inputElement = getByPlaceholderText('New Feature Key');
+    fireEvent.changeText(inputElement, 'New Feature Key');
+    await waitFor(() => expect(inputElement.props.value).toBe('New Feature Key'));
   });
   it('should update the status when the selected value changes', async () => {
     const dispatch = jest.fn();
     (useAppDispatch as unknown as jest.Mock).mockReturnValue(dispatch);
     const { getByTestId, getByText } = renderWithTamagui(<AddFeature />);
-    await act(async () => {
-      const select = getByTestId('select-status');
-      expect(select).toBeTruthy();
-      fireEvent(select, 'onValueChange', 'ACTIVE');
-      await waitFor(() => expect(select).toHaveTextContent('ACTIVE'));
-      const addButton = getByText('Add');
-      fireEvent.press(addButton);
-      expect(dispatch).toHaveBeenCalledWith(expect.objectContaining({
-        type: 'features/featureAdded',
-        payload: expect.objectContaining({
-          status: 'ACTIVE',
-        }),
-      }));
-    });
+    const select = getByTestId('select-status');
+    expect(select).toBeTruthy();
+    fireEvent(select, 'onValueChange', 'ACTIVE');
+    await waitFor(() => expect(select).toHaveTextContent('ACTIVE'));
+    const addButton = getByText('Add');
+    fireEvent.press(addButton);
+    expect(dispatch).toHaveBeenCalledWith(expect.objectContaining({
+      type: 'features/featureAdded',
+      payload: expect.objectContaining({
+        status: 'ACTIVE',
+      }),
+    }));
   });
   it('should dispatch featureAdded action when Add button is pressed', async () => {
     const dispatch = jest.fn();
