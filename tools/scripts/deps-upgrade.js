29755172ecdc7686bf64d720a9e3a084b0f6064c
Stewart Armbrecht (BBB) <stewart@boundbybetter.com>
2024-08-24 12:38:56 +0000

    yarn deps:upgrade Also expo upgrade partially done.

--- tools/scripts/deps-upgrade.js
+++ tools/scripts/deps-upgrade.js
@@ -3,10 +3,9 @@ const fs = require('fs');
 const path = require('path');
 const glob = require('glob');
 
-// Get the dependencies to check and upgrade from command line arguments
+// Get the list of dependencies to check and upgrade from command-line arguments
 const dependenciesToCheck = process.argv.slice(2);
 
-// Check if any dependencies were provided
 if (dependenciesToCheck.length === 0) {
   console.error(
     'Please provide one or more dependencies to check and upgrade.',
@@ -14,48 +13,43 @@ if (dependenciesToCheck.length === 0) {
   process.exit(1);
 }
 
-// Get the path to the package.json file
+// Path to the root package.json file
 const packageJsonPath = path.resolve('package.json');
-
-// Read the package.json file
 const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
 
-// Get the workspaces from package.json
+// Get the list of workspaces from the package.json file
 const workspaces = packageJson.workspaces;
 
-// Check if workspaces are defined
 if (!workspaces) {
   console.error('No workspaces found in package.json.');
   process.exit(1);
 }
 
-// Resolve workspace paths using glob
+// Resolve workspace paths using glob patterns
 const workspacePaths = workspaces.flatMap((workspacePattern) =>
   glob.sync(workspacePattern),
 );
 
 // Function to check and upgrade dependencies in a given package.json path
 const checkAndUpgradeDependencies = (packageJsonPath, workspaceName) => {
-  // Check if the package.json file exists
   if (fs.existsSync(packageJsonPath)) {
-    // Read the package.json file
     const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
     const dependencies = packageJson.dependencies || {};
     const devDependencies = packageJson.devDependencies || {};
 
-    // Iterate over the dependencies to check and upgrade
     dependenciesToCheck.forEach((dependencyWithVersion) => {
       const [dependency, version] = dependencyWithVersion.split('@');
-      // Check if the dependency exists in either dependencies or devDependencies
       if (dependencies[dependency] || devDependencies[dependency]) {
-        const upgradeCommand = version
+        const installCommand = version
           ? `${dependency}@${version}`
           : dependency;
         console.log(
-          `Upgrading ${dependency} in ${workspaceName} to version ${version || 'latest'}...`,
+          `Upgrading ${dependency} in ${workspaceName} to version ${version || 'latest'} using expo install...`,
         );
-        // Execute the upgrade command using yarn
-        execSync(`yarn upgrade ${upgradeCommand}`, { stdio: 'inherit' });
+        execSync(`npx expo install ${installCommand}`, {
+          stdio: 'inherit',
+          cwd: path.dirname(packageJsonPath), // Set the working directory to the workspace
+        });
       } else {
         console.log(`${dependency} not found in ${workspaceName}.`);
       }
